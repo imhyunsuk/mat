21d44ee93fe6aa15bd6fcac375234d19c627c86c
ian <better.imhs@gmail.com>
2019-11-20 21:50:53 +0900

    Get current position before setting map
    * Current position can be used only in https env

--- components/Map.js
+++ components/Map.js
@@ -1,40 +1,58 @@
-import React, {useEffect} from 'react'
+import React, {useState, useEffect} from 'react'
 
 const Map = ({ stores }) => {
-    useEffect(() => {
-        var container = document.getElementById('map');
-        var mapOptions = {
-            center: new kakao.maps.LatLng(33.450701, 126.570667),
-            level: 3, //지도의 레벨(확대, 축소 정도)
-            clickable: true,
-        }
-        var map = new kakao.maps.Map(container, mapOptions); //지도 생성 및 객체 리턴
+  const [position, setPosition] = useState({ "latitude": 33.450701, "longitude": 126.570667});
 
-        stores.map((store) => {
-          var marker = new kakao.maps.Marker({
-            map: map,
-            position: new kakao.maps.LatLng(store.lat, store.lng),
-            title: store.name, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-          });
-        })
-    }, [stores]);
+  useEffect(() => {
+    if (navigator.geolocation) { 
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setPosition({"latitude": position.coords.latitude, "longitude": position.coords.longitude});
+      }, function (error) {
+        console.error(error);
+      }, {
+        enableHighAccuracy: false,
+        maximumAge: 0,
+        timeout: Infinity
+      });
+    } else {
+      alert('GPS를 지원하지 않습니다');
+    }
 
-    return (
-      <div>
-        <div id="map">Map</div>
+    var container = document.getElementById('map');
+    var mapOptions = {
+        center: new kakao.maps.LatLng(position.latitude, position.longitude),
+        level: 10, //지도의 레벨(확대, 축소 정도)
+        clickable: true,
+    }
+    var map = new kakao.maps.Map(container, mapOptions); //지도 생성 및 객체 리턴
 
-        <style jsx>
-          {`
-            #map {
-              float: left;
-              width: 70%;
-              height: 100vh;
-              padding: 3px;
-            }
-          `}
-        </style>
-      </div>
-    );
+    stores.map((store) => {
+      var marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(store.lat, store.lng),
+        title: store.name, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+      });
+    })
+  }, [stores]);
+  
+
+
+  return (
+    <div>
+      <div id="map">Map</div>
+
+      <style jsx>
+        {`
+          #map {
+            float: left;
+            width: 70%;
+            height: 100vh;
+            padding: 3px;
+          }
+        `}
+      </style>
+    </div>
+  );
     
 }
 
